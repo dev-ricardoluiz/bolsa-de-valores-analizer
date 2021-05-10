e05154dbfc07ad9e3aefae37d75e9cf9b0f9b58f
Ricardo Luiz de Carvalho Barbosa <ricardoluiz-97@hotmail.com>
2021-05-10 11:51:54 -0300

    arquivos

--- acao-especifica.py
+++ acao-especifica.py
@@ -16,17 +16,23 @@ from email.message import EmailMessage
 import re
 import time
 from time import sleep
+
+
+#input para especificar ticket da ação que deseja avaliar
+
+acao = input('Digite o Ticket (Ex.: PETR3) da Ação que deseja analizar: ')
+
 browser = webdriver.Chrome(ChromeDriverManager().install())
 
-#Array com todas as ações para avaliar
-acao = ["LWSA3", "CSNA3", "ITSA4", "BBSE3", "SQIA3", "PETR3", "PETR4"]
+print('Analisando ação', acao)
 
-total = len(acao)
-posicao = 0
-for c in range (0, total):
-    browser.get('https://statusinvest.com.br/acoes/'+acao[posicao])
-    print('Ação', acao[posicao])
-    pl = browser.find_element_by_xpath('//*[@id="indicators-section"]/div[2]/div/div[1]/div/div[2]/div/div/strong').text
-    print('P/L da Ação', acao[posicao], 'é', pl)
-    posicao = posicao + 1
-    time.sleep(5)
+try:
+    browser.get('https://statusinvest.com.br/acoes/'+acao)
+    #tenta pegar o P/L da ação
+    try:
+        pl = browser.find_element_by_xpath('//*[@id="indicators-section"]/div[2]/div/div[1]/div/div[2]/div/div/strong').text
+        print('P/L da Ação', acao, 'é', pl,'.')
+    except NoSuchElementException:
+        print('Não foi possível obter o P/L da ação', acao)
+except NoSuchElementException:
+    print('Não foi possível encontrar essa ação. Tem certeza que digitou o Ticket corretamente?')
