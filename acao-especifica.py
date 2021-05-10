16882240f1b15b4a46380e4e6aabcbd035931f7c
Ricardo Luiz de Carvalho Barbosa <ricardoluiz-97@hotmail.com>
2021-05-10 12:38:54 -0300

    Update acao-especifica.py

--- acao-especifica.py
+++ acao-especifica.py
@@ -17,22 +17,52 @@ import re
 import time
 from time import sleep
 
-
 #input para especificar ticket da ação que deseja avaliar
 
-acao = input('Digite o Ticket (Ex.: PETR3) da Ação que deseja analizar: ')
+ticker = input('Digite o Ticket (Ex.: PETR3) da Ação que deseja analizar: ')
 
 browser = webdriver.Chrome(ChromeDriverManager().install())
 
-print('Analisando ação', acao)
 
 try:
-    browser.get('https://statusinvest.com.br/acoes/'+acao)
-    #tenta pegar o P/L da ação
+    browser.get('https://statusinvest.com.br/acoes/'+ticker)
     try:
+        #DESCRIÇÃO DA AÇÃO
+        acao = browser.find_element_by_xpath('//*[@id="main-header"]/div/div/div[1]/h1/small').text
+        print('')
+        print('Analisando ação', ticker, '-', acao)
+        print('')
+
+        #COTAÇÃO
+        cotacao = browser.find_element_by_xpath('//*[@id="main-2"]/div[2]/div/div[1]/div/div[1]/div/div[1]/strong').text
+        print('Cotação:', cotacao)
+
+        #P/L
         pl = browser.find_element_by_xpath('//*[@id="indicators-section"]/div[2]/div/div[1]/div/div[2]/div/div/strong').text
-        print('P/L da Ação', acao, 'é', pl,'.')
+        print('P/L:', pl)
+
+        #D.Y.
+        dy = browser.find_element_by_xpath('//*[@id="indicators-section"]/div[2]/div/div[1]/div/div[1]/div/div/strong').text
+        print('Dividend Yield:', dy)
+
+        #PEG RATIO
+        #P/VP
+        #EV/EBITDA
+        #EV/EBIT
+        #P/EBITDA
+        #P/EBIT
+        #VPA
+        #P/ATIVO
+        #LPA
+        #P/SR
+        #P/CAP.GIRO
+        #P/ATIVO CIRCULANTE LÍQUIDO
+        #
+        #
+        #
+
+        #Após mostrar todos os indicadores da ação fazemos algumas análises
     except NoSuchElementException:
-        print('Não foi possível obter o P/L da ação', acao)
+        print('Não foi possível obter os dados da ação', ticker)
 except NoSuchElementException:
     print('Não foi possível encontrar essa ação. Tem certeza que digitou o Ticket corretamente?')
