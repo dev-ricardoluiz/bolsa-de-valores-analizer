732d247fbd387a303951d8823aa17030a17495b1
Ricardo Luiz de Carvalho Barbosa <ricardoluiz-97@hotmail.com>
2021-05-20 21:09:40 -0300

    Update um_porcento_ao_mes.py

--- um_porcento_ao_mes.py
+++ um_porcento_ao_mes.py
@@ -10,11 +10,5 @@ for c in range (0, duracao):
     print('No mês', mes, 'você terá: R$', saldo)
     mes = mes + 1
 print('')
-print('Ao final de', mes, 'meses (aproximadamente', mes/12, 'anos, investindo mensalmente R$', aporte, 'com uma rentabilidade média de', rentabilidade, '%' ' ao mês, você terá R$', saldo, 'em seu saldo. Seja um investidor!')
-
-ações	cotação	qtd	qtd	total
-wizs3	12,15	100	64	1992,6
-beef3	9,49	200	10	1992,9
-cebr3	192,33	0	11	2115,63
-trpl4	26,19	0	76	1990,44
-csmg3	17,48	100	15	2010,2
+print('Ao final de', mes-1, 'meses (aproximadamente', (mes-1)/12, 'anos) investindo mensalmente R$', aporte, 'com uma rentabilidade média de', rentabilidade, '%' ' ao mês, você terá R$', saldo, 'em seu saldo. Seja um investidor!')
+print('')
